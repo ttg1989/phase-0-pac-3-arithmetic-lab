727bb1cb55e41e8c519b6652b2871bf5e52916af
Christopher Sloss <ttg1989_89@protonmail.com>
2022-10-01 17:13:04 -0400

    commit tree e8e56029352304e2347e45ced2f02734115713fc

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b) // Add two numbers together and return the result.
+{
+    return a+b;
+}
+
+function subtract(a,b) // Subtract two numbers together and return the result.
+{
+    return a-b;
+}
+
+function multiply(a,b) // Multiply two numbers together and return the result.
+{
+    return a*b;
+}
+
+function divide(a,b) // Divide two numbers together and return the result.
+{
+    return a/b;
+}
+
+function increment(n) // Increment a number and return the result.
+{
+    n++;
+    return n;
+}
+
+function decrement(n) // Decrement a number and return the result.
+{
+    n--
+    return n;
+}
+
+function makeInt(n) // Parse n to a radix of 10.
+{
+    return parseInt(n,10);
+}
+
+function preserveDecimal(n) // Parse n as a float.
+{
+    return parseFloat(n);
+}
